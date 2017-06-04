c8fc67f754fe7bf7d5c4bd86b132d39c6d039b3d
Your Name <you@example.com>
2017-06-04 23:14:06 +0000

    Run lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to frp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/frp/core_test.clj
@@ -0,0 +1,7 @@
+(ns frp.core-test
+  (:require [clojure.test :refer :all]
+            [frp.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
