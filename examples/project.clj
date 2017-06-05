368150de924480aa20b4acc88cd80fb086c63fa6
Your Name <you@example.com>
2017-06-05 08:00:06 +0000

    Configure advanced compilation

--- examples/project.clj
+++ examples/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.494"]
                  [bidi "2.1.0"]
                  [cljs-ajax "0.6.0"]
                  [frp "0.1.0"]
@@ -11,6 +12,14 @@
                  [reagent "0.6.1"]]
   :main ^:skip-aot examples.core
   :target-path "target/%s"
+  :plugins [[lein-ancient "0.6.10"]
+            [lein-cljsbuild "1.1.3"]]
+  :cljsbuild {:builds
+              {:prod
+               {:source-paths ["src"]
+                :compiler     {:output-to     "dist/js/main.js"
+                               :main          examples.core
+                               :optimizations :advanced}}}}
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.2"]
                                   [com.taoensso/encore "2.90.1"]
                                   [figwheel-sidecar "0.5.9"]
