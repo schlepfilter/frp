137fd05652ce3fce74a6c2f9f10dc7bfedf4b09c
Luke Sugiura <jun.sugiura.jp@gmail.com>
2017-06-05 21:55:45 +0900

    add reverse-text example

---
+++ examples/src/examples/cycle/reverse-text.cljs
@@ -0,0 +1,33 @@
+(ns examples.cycle.reverse-text
+  (:require [help.core :as help]
+            [help.unit :as unit]
+            [frp.clojure.core :as core]
+            [frp.core :as frp]))
+
+(def key-stroke
+  (frp/event))
+
+(def reversed-text
+  (->> key-stroke
+       (help/<$> reverse)
+       (frp/stepper "")))
+
+(def style
+  {:width "100%"
+   :height "40px"
+   :padding "10px 0"
+   :text-align "center"
+   :font-size "2em"
+   :font-family "Helvetica"})
+
+(defn text-component
+  [reversed-text*]
+  [:div
+   [:input {:on-change #(-> % .-target .-value key-stroke)
+                  :type      "text"
+                  :placeholder "Text to reverse"
+                  :style style}]
+   [:p {:style style} reversed-text*]])
+
+(def reverse-text
+  (help/<$> text-component reversed-text))
