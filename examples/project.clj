7c37bbe5bf682ec0161ab9f91a6297b0b4f5bd6c
Your Name <you@example.com>
2018-12-06 06:35:17 +0000

    Disable protect

--- examples/project.clj
+++ examples/project.clj
@@ -8,7 +8,7 @@
                  [reagent "0.8.1"]]
   :plugins [[lein-ancient "0.6.15"]
             [lein-cljsbuild "1.1.7"]]
-  :clean-targets ["resources/public/js"]
+  :clean-targets ^{:protect false} ["resources/public/js"]
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [figwheel-sidecar "0.5.17"]
                                   [org.clojure/tools.namespace "0.2.11"]
