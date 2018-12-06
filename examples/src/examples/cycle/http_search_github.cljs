ce9ffa778e5268a0f2ee88ca817a5cf6b37d0ac6
Your Name <you@example.com>
2018-12-06 08:05:05 +0000

    Reorder dependencies

--- examples/src/examples/cycle/http_search_github.cljs
+++ examples/src/examples/cycle/http_search_github.cljs
@@ -1,9 +1,9 @@
 (ns examples.cycle.http-search-github
   (:require [clojure.walk :as walk]
             [cats.core :as m]
+            [frp.ajax :refer [GET]]
             [frp.clojure.core :as core]
-            [frp.core :as frp]
-            [frp.ajax :refer [GET]]))
+            [frp.core :as frp]))
 
 (def term
   (frp/event))
