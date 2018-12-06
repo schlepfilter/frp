4f38dba2dda2447a13e5f7f14a7cf1a065fdbd79
Your Name <you@example.com>
2018-12-06 07:03:24 +0000

    Push the compiled code

--- script/circleci.sh
+++ script/circleci.sh
@@ -11,7 +11,9 @@ lein doo chrome-headless test once &&
 lein install &&
 cd examples &&
 rm -rf resources/public &&
-git clone https://github.com/frpexamples/frpexamples.github.io.git resources/public &&
+git clone "https://schlepfilter:$GITHUB_TOKEN@github.com/frpexamples/frpexamples.github.io.git" resources/public &&
 git checkout -- . &&
 lein clean &&
-lein cljsbuild once prod
+lein cljsbuild once prod &&
+cd resources/public &&
+git push origin master
