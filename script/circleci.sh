942b1258e311c398d53f9b30c6d909c5835b6d10
Your Name <you@example.com>
2018-12-09 01:24:18 +0000

    Use another account for deployment

--- script/circleci.sh
+++ script/circleci.sh
@@ -11,7 +11,7 @@ lein doo chrome-headless test once &&
 lein install &&
 cd examples &&
 rm -rf resources/public &&
-git clone "https://schlepfilter:$GITHUB_TOKEN@github.com/frpexamples/frpexamples.github.io.git" resources/public &&
+git clone "https://schlepfilterdeployment:$GITHUB_TOKEN@github.com/frpexamples/frpexamples.github.io.git" resources/public &&
 git checkout -- . &&
 lein clean &&
 lein cljsbuild once prod &&
