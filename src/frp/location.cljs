43bb5ab5dc47dc680d11f02629f7bd254f05c48b
Your Name <you@example.com>
2018-12-10 07:49:09 +0000

    Use defbehavior

--- src/frp/location.cljs
+++ src/frp/location.cljs
@@ -1,15 +1,12 @@
 (ns frp.location
   (:require [cats.core :as m]
+            [frp.browser :as browser :include-macros true]
             [frp.history :as history]
             [frp.primitives.behavior :as behavior :include-macros true]
             [frp.window :as window]))
 
-(def pathname
-  (behavior/->Behavior ::pathname))
-
-(behavior/register
-  (behavior/redef pathname
-                  (->> (m/<> window/popstate history/pushstate)
-                       (m/<$> (comp :pathname
-                                    :location))
-                       (behavior/stepper js/location.pathname))))
+(browser/defbehavior pathname
+  #(->> (m/<> window/popstate history/pushstate)
+        (m/<$> (comp :pathname
+                     :location))
+        (behavior/stepper js/location.pathname)))
