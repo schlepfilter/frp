da49f3ab72904e8d145a3af8a9f8cd438bc4c99d
Your Name <you@example.com>
2018-12-11 12:17:43 +0000

    Use defcurried for request

--- src/frp/ajax.cljs
+++ src/frp/ajax.cljs
@@ -1,16 +1,16 @@
 (ns frp.ajax
-  (:require [ajax.core :as ajax]
+  (:require [aid.core :as aid]
+            [ajax.core :as ajax]
             [com.rpl.specter :as s :include-macros true]
             [frp.browser :as browser]
             [frp.derived :as derived]))
 
-(defn make-request
-  [f]
-  (fn [url option]
-    (browser/effect #(->> option
-                          (s/setval :handler %)
-                          (f url))
-                    (derived/event))))
+(aid/defcurried request
+                [f url option]
+                (browser/effect #(->> option
+                                      (s/setval :handler %)
+                                      (f url))
+                                (derived/event)))
 
 (def GET
-  (make-request ajax/GET))
+  (request ajax/GET))
