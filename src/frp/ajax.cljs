2165388cccf0695721e90a43eed3213e5df67ef5
Your Name <you@example.com>
2018-12-11 12:42:39 +0000

    Regress

--- src/frp/ajax.cljs
+++ src/frp/ajax.cljs
@@ -1,16 +1,17 @@
 (ns frp.ajax
-  (:require [aid.core :as aid]
-            [ajax.core :as ajax]
+  (:require [ajax.core :as ajax]
             [com.rpl.specter :as s :include-macros true]
             [frp.browser :as browser]
             [frp.derived :as derived]))
 
-(aid/defcurried request
-  [f url option]
-  (browser/effect #(->> option
-                        (s/setval :handler %)
-                        (f url))
-                  (derived/event)))
+(defn make-request
+  [f]
+  (fn [url option]
+    (browser/effect #(->> option
+                          (merge {:handler identity})
+                          (s/transform :handler (partial comp %))
+                          (f url))
+                    (derived/event))))
 
 (def GET
-  (request ajax/GET))
+  (make-request ajax/GET))
