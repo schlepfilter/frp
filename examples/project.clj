eab58662149f25c5a228dc92ae2ea2ce13becaef
Your Name <you@example.com>
2018-12-12 07:28:48 +0000

    Attach workspace after cloning the repository

--- examples/project.clj
+++ examples/project.clj
@@ -9,7 +9,6 @@
                  [thi.ng/color "1.3.0"]]
   :plugins [[lein-ancient "0.6.15"]
             [lein-cljsbuild "1.1.7"]]
-  :clean-targets ^{:protect false} ["resources/public/js" :target-path]
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.10"]
                                   [figwheel-sidecar "0.5.17"]
                                   [org.clojure/tools.namespace "0.2.11"]
--- script/circleci/integrate.sh
+++ script/circleci/integrate.sh
@@ -10,8 +10,4 @@ lein npm install &&
 lein doo chrome-headless test once &&
 lein install &&
 cd examples &&
-rm -rf resources/public &&
-git clone "https://schlepfilterdeployment:$GITHUB_TOKEN@github.com/frpexamples/frpexamples.github.io.git" resources/public &&
-git checkout -- . &&
-lein clean &&
 lein cljsbuild once prod
