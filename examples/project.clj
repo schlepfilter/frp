11679cc9526ccad1b8af98b98cb171821a57a9f5
Your Name <you@example.com>
2018-12-18 23:30:03 +0000

    Update clojure in examples

--- examples/project.clj
+++ examples/project.clj
@@ -1,5 +1,5 @@
 (defproject examples "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]
                  [aid "0.1.2"]
                  [bidi "2.1.4"]
