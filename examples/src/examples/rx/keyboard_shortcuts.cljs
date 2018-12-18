7abed99c882602846b486f04c4c07e6f98e5a180
Your Name <you@example.com>
2018-12-18 06:21:29 +0000

    Implement counter

--- examples/src/examples/rx/keyboard_shortcuts.cljs
+++ examples/src/examples/rx/keyboard_shortcuts.cljs
@@ -1,7 +1,11 @@
 (ns examples.rx.keyboard-shortcuts
   (:require [cats.core :as m]
             [cljsjs.mousetrap]
-            [frp.core :as frp]))
+            [com.rpl.specter :as s]
+            [frp.core :as frp]
+            [frp.clojure.core :as core]
+            [aid.core :as aid]
+            [clojure.string :as str]))
 
 (def typing
   (frp/event))
@@ -12,22 +16,34 @@
 (def trigger
   (frp/event))
 
+(def counter
+  (->> trigger
+       (m/<> registration)
+       (core/group-by identity)
+       (m/<$> (partial s/transform* s/MAP-VALS (comp dec
+                                                     count)))))
+
 (defn keyboard-shortcuts-component
-  [x]
+  [typing* counter*]
   [:div
    [:input {:type        "text"
             :placeholder "ctrl+alt+d"
-            :value       x
+            :value       typing*
             :on-change   #(-> %
                               .-target.value
                               typing)}]
-   [:button {:on-click #(registration x)}
+   [:button {:on-click #(registration typing*)}
     "Add"]
    [:p "Keyboard shortcuts"]
-   [:ul]])
+   (->> counter* (s/transform s/MAP-VALS str)
+        (mapv (comp (partial vector :li)
+                    (partial str/join ": ")))
+        (s/setval s/BEFORE-ELEM :ul))])
 
 (def keyboard-shortcuts
-  (m/<$> keyboard-shortcuts-component (frp/stepper "" typing)))
+  ((aid/lift-a keyboard-shortcuts-component)
+    (frp/stepper "" typing)
+    (frp/stepper {} counter)))
 
 (frp/on (fn [registration*]
           (js/Mousetrap.bind registration* #(trigger registration*)))
